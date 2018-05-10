67e172154ccff9793c1716e1cae4c9fec7570c14
c0reac0de <candice.correa@gmail.com>
2018-05-10 17:39:42 -0400

    second

---
+++ w5wordguess.js
@@ -0,0 +1,28 @@
+
+// Game counters
+var winCounter = 0;
+var lossCounter = 0;
+
+
+// FUNCTIONS 
+
+// startGame function
+
+  //on click 
+        //stop click function for other choices
+        //record choice
+        //store answer in correct or incorrect array
+
+        //if user answer === true answer, add to win counter
+        //else add to lose counter
+
+//setInterval function
+
+    //display timer in Stopwatch div
+    //var clockRunning=false
+    //= stopWatch{ $(display).text(120:00);}
+    
+//stop function
+//if the display hits 00:00 again
+//alert time's up
+//display results
